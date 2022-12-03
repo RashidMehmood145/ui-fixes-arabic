c596ae1ece5bc73056b03b35d11e151f7cfbf06b
ahtishambashir <ahtishamdoggar@gmail.com>
2022-12-03 23:55:48 +0500

    trade page

--- ar/ICAP-trade.php
+++ ar/ICAP-trade.php
@@ -5,115 +5,115 @@ include('header.php');
 			<section class="icap-fund-hero-section">
 				<img src="./assets/images/icap-fund-banner.svg">
 			</section>
-			<section class="after-icap-fund-hero-section">
+			<section class="after-icap-fund-hero-section trade-arabic">
 				<div class="container-fluid">
 					<div class="row">
 						<div class="content-outer-line-container">
 							<div class="inner-col">
 								<div class="row">
-									<div class="icap-fund-package-table-col">
+									<div class="col-xl-3 mb-3 col-md-6">
 										<div class="icap-fund-type-wrapper">
-											<div class="icap-fund-type-header">
+											<div class="icap-fund-type-header rtl">
 												<span class="yellow-line">Alistithmar Capital Saudi</span> <br><span class="yellow-line">Companies Fund</span> 
 											</div>
 											<div class="icap-fund-type-content-list">
-												<ul class="icap-fund-list-item">
-													<li>Fund In SAR</li>
-													<li>Daily Valuation</li>
-													<li>12 Months Performance 7.36</li>
-													<li>Benchmark S&P Saudi Arabia Shariah (SAUDI RIYAL)</li>
-													<li>Fund In SAR</li>
-													<li>Daily Valuation</li>
-													<li>12 Months Performance 7.36</li>
-													<li>Benchmark S&P Saudi Arabia Shariah (SAUDI RIYAL)</li>
+												<ul class="icap-fund-list-item rtl">
+													<li class="rtl">Fund In SAR</li>
+													<li class="rtl">Daily Valuation</li>
+													<li class="rtl">12 Months Performance 7.36</li>
+													<li class="rtl">Benchmark S&P Saudi Arabia Shariah (SAUDI RIYAL)</li>
+													<li class="rtl">Fund In SAR</li>
+													<li class="rtl">Daily Valuation</li>
+													<li class="rtl">12 Months Performance 7.36</li>
+													<li class="rtl">Benchmark S&P Saudi Arabia Shariah (SAUDI RIYAL)</li>
 												</ul>
 												<div class="history-tc-button-wrapper">
-													<a href="#" class="price-history-cta">Price History</a>
-													<a href="#" class="term-conduction-cta">T & C</a>
+													<a href="#" class="price-history-cta rtl">Price History</a>
+													<a href="#" class="term-conduction-cta rtl">T & C</a>
 												</div>
 												<div class="subscribe-button-wrapper">
-													<a href="#" class="subscribe-cta">Subscribe</a>
+													<a href="#" class="subscribe-cta rtl">Subscribe</a>
 												</div>
 												
 											</div>
 										</div>
 									</div><!------------End col------------>
-									<div class="icap-fund-package-table-col">
+									<div class="col-xl-3 mb-3 col-md-6">
 										<div class="icap-fund-type-wrapper">
-											<div class="icap-fund-type-header">
+											<div class="icap-fund-type-header rtl">
 												<span class="yellow-line">Alistithmar Capital SAR</span> <br><span class="yellow-line">Murabaha Fund</span> 
 											</div>
 											<div class="icap-fund-type-content-list">
-												<ul class="icap-fund-list-item">
-													<li>Fund In SAR</li>
-													<li>Daily Valuation</li>
-													<li>12 Months Performance 7.36</li>
-													<li>Benchmark S&P Saudi Arabia Shariah (SAUDI RIYAL)</li>
-													<li>Fund In SAR</li>
-													<li>Daily Valuation</li>
-													<li>12 Months Performance 7.36</li>
-													<li>Benchmark S&P Saudi Arabia Shariah (SAUDI RIYAL)</li>
+												<ul class="icap-fund-list-item rtl">
+													<li class="rtl">Fund In SAR</li>
+													<li class="rtl">Daily Valuation</li>
+													<li class="rtl">12 Months Performance 7.36</li>
+													<li class="rtl">Benchmark S&P Saudi Arabia Shariah (SAUDI RIYAL)</li>
+													<li class="rtl">Fund In SAR</li>
+													<li class="rtl">Daily Valuation</li>
+													<li class="rtl">12 Months Performance 7.36</li>
+													<li class="rtl">Benchmark S&P Saudi Arabia Shariah (SAUDI RIYAL)</li>
 												</ul>
 												<div class="history-tc-button-wrapper">
-													<a href="#" class="price-history-cta">Price History</a>
-													<a href="#" class="term-conduction-cta">T & C</a>
+													<a href="#" class="price-history-cta rtl">Price History</a>
+													<a href="#" class="term-conduction-cta rtl">T & C</a>
 												</div>
 												<div class="subscribe-button-wrapper">
-													<a href="#" class="subscribe-cta">Subscribe</a>
+													<a href="#" class="subscribe-cta rtl">Subscribe</a>
 												</div>
 												
 											</div>
 										</div>
 									</div><!------------End col------------>
-									<div class="icap-fund-package-table-col">
+									<div class="col-xl-3 mb-3 col-md-6">
 										<div class="icap-fund-type-wrapper">
-											<div class="icap-fund-type-header">
+											<div class="icap-fund-type-header rtl">
 												<span class="yellow-line">Alistithmar Capital GCC</span> <br><span class="yellow-line">Equity Fund</span>
 											</div>
 											<div class="icap-fund-type-content-list">
-												<ul class="icap-fund-list-item">
-													<li>Fund In SAR</li>
-													<li>Daily Valuation</li>
-													<li>12 Months Performance 7.36</li>
-													<li>Benchmark S&P Saudi Arabia Shariah (SAUDI RIYAL)</li>
-													<li>Fund In SAR</li>
-													<li>Daily Valuation</li>
-													<li>12 Months Performance 7.36</li>
-													<li>Benchmark S&P Saudi Arabia Shariah (SAUDI RIYAL)</li>
+												<ul class="icap-fund-list-item rtl">
+													<li class="rtl">Fund In SAR</li>
+													<li class="rtl">Daily Valuation</li>
+													<li class="rtl">12 Months Performance 7.36</li>
+													<li class="rtl">Benchmark S&P Saudi Arabia Shariah (SAUDI RIYAL)</li>
+													<li class="rtl">Fund In SAR</li>
+													<li class="rtl">Daily Valuation</li>
+													<li class="rtl">12 Months Performance 7.36</li>
+													<li class="rtl">Benchmark S&P Saudi Arabia Shariah (SAUDI RIYAL)</li>
 												</ul>
 												<div class="history-tc-button-wrapper">
-													<a href="#" class="price-history-cta">Price History</a>
-													<a href="#" class="term-conduction-cta">T & C</a>
+													<a href="#" class="price-history-cta rtl">Price History</a>
+													<a href="#" class="term-conduction-cta rtl">T & C</a>
 												</div>
 												<div class="subscribe-button-wrapper">
-													<a href="#" class="subscribe-cta">Subscribe</a>
+													<a href="#" class="subscribe-cta rtl">Subscribe</a>
 												</div>
 												
 											</div>
 										</div>
 									</div><!------------End col------------>
-									<div class="icap-fund-package-table-col">
+									<div class="col-xl-3 mb-3 col-md-6">
 										<div class="icap-fund-type-wrapper">
-											<div class="icap-fund-type-header">
+											<div class="icap-fund-type-header rtl">
 												<span class="yellow-line">Alistithmar Capital Freestyle</span>  <br><span class="yellow-line">Saudi Equity Fund</span> 
 											</div>
 											<div class="icap-fund-type-content-list">
-												<ul class="icap-fund-list-item">
-													<li>Fund In SAR</li>
-													<li>Daily Valuation</li>
-													<li>12 Months Performance 7.36</li>
-													<li>Benchmark S&P Saudi Arabia Shariah (SAUDI RIYAL)</li>
-													<li>Fund In SAR</li>
-													<li>Daily Valuation</li>
-													<li>12 Months Performance 7.36</li>
-													<li>Benchmark S&P Saudi Arabia Shariah (SAUDI RIYAL)</li>
+												<ul class="icap-fund-list-item rtl">
+													<li class="rtl">Fund In SAR</li>
+													<li class="rtl">Daily Valuation</li>
+													<li class="rtl">12 Months Performance 7.36</li>
+													<li class="rtl">Benchmark S&P Saudi Arabia Shariah (SAUDI RIYAL)</li>
+													<li class="rtl">Fund In SAR</li>
+													<li class="rtl">Daily Valuation</li>
+													<li class="rtl">12 Months Performance 7.36</li>
+													<li class="rtl">Benchmark S&P Saudi Arabia Shariah (SAUDI RIYAL)</li>
 												</ul>
 												<div class="history-tc-button-wrapper">
-													<a href="#" class="price-history-cta">Price History</a>
-													<a href="#" class="term-conduction-cta">T & C</a>
+													<a href="#" class="price-history-cta rtl">Price History</a>
+													<a href="#" class="term-conduction-cta rtl">T & C</a>
 												</div>
 												<div class="subscribe-button-wrapper">
-													<a href="#" class="subscribe-cta">Subscribe</a>
+													<a href="#" class="subscribe-cta rtl">Subscribe</a>
 												</div>
 												
 											</div>
